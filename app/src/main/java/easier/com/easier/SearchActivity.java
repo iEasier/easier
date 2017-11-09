77022fea145a5f5f101a2eecf79f9eb6ac96f65d
iEasier <easiermail@163.com>
2017-11-09 09:03:43 +0800

    【修改人】方亮 【Defect】N/A 【Story】N/A 【修改原因】 添加带码

---
+++ app/src/main/java/easier/com/easier/SearchActivity.java
@@ -0,0 +1,11 @@
+package easier.com.easier;
+
+import android.os.Bundle;
+
+public class SearchActivity extends TopBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        showSearchInput();
+    }
+}
